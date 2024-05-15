9d6ad7e6b198cef4f3f739e70b5cf837ba796c84
piyushnksingh <piyushnksingh@gmail.com>
2024-05-15 20:01:18 +0530

    Added css and watched till #9

--- text_utils/text_utils/views.py
+++ text_utils/text_utils/views.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponse
+from django.shortcuts import render 
 
 def index(request):
-    return HttpResponse("Home")
+    return render(request,"text_utils/index.html")
 
 def remove_punc(request):
+    print(request.GET.get("text","default"))
     return HttpResponse("remove_punc")
 
 def capitalize_first(request):
